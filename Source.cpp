9adaed86a62bfb212347a663734c9bab85b5da2c
xReggimon <r-mcmullen.1@onu.edu>
2024-03-27 17:07:36 -0400

    Completed countVowel Function.

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,18 @@
 
 using namespace std;
 
-int countCharacter(string str) {
-	return str.length();
+bool trueVowel(char vowel) {
+	return vowel == 'a' || vowel == 'A' || vowel == 'e' || vowel == 'E' || vowel == 'i' || vowel == 'I' || vowel == 'o' || vowel == 'O' || vowel == 'u' || vowel == 'U';
+}
+
+int countVowel(string str) {
+	int countVowel = 0;
+	for (char vowel : str) {
+		if (trueVowel(vowel)) {
+			countVowel++;
+		}
+	}
+	return countVowel;
 }
 
 int main() {
@@ -13,7 +23,7 @@ int main() {
 	cout << "Enter a word or number: ";
 	getline(cin, str);
 
-	cout << "Number of characters: " << countCharacter(str) << endl;
+	cout << "Number of vowels: " << countVowel(str) << endl;
 
 	return 0;	
 }
