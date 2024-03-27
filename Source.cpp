1c0b178f7016bd5763f5f8d8e4145ec93e41a6e7
xReggimon <r-mcmullen.1@onu.edu>
2024-03-27 16:46:34 -0400

    Completed countCharacter Function.

--- Source.cpp
+++ Source.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+int countCharacter(string str) {
+	return str.length();
+}
+
+int main() {
+
+	string str;
+	cout << "Enter a word or number: ";
+	getline(cin, str);
+
+	cout << "Number of characters: " << countCharacter(str) << endl;
+
+	return 0;	
+}
